9bba04ccf4f209eb5e2bfb406fb455953b369387
Daniel McNamee <danmc@Daniels-MacBook-Pro.local>
2017-04-23 13:37:28 +0100

    init traj probability calc'

---
+++ TrajectoryProbability.m
@@ -0,0 +1,18 @@
+function P = TrajectoryProbability(TX,L,K,A,B,H,varargin)
+%% FUNCTION: Probability of trajectory TX under control law L and sensory filter K.
+% INPUTS:   TX          = time x state input matrix
+%           L           = control law
+%           K           = Sensory filter
+%           A           = state-evolution matrix
+%           B           = control-input matrix
+%           H           = sensory-indicator matrix
+% OUTPUTS:  P           = state probability as a function of time
+% NOTES:    Viterbi path...
+% ISSUES:   To do.
+% REFS:     N/A
+% AUTHOR:   Daniel McNamee, daniel.c.mcnamee@gmail.com
+
+
+
+
+end
